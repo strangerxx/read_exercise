bfb02725166a1370336256be56222c18fd337ab0
Artur Davletov <artur.davletov.m@gmail.com>
2016-06-01 10:03:14 +0500

    remove non ascii symbols

--- get_exercises.rb
+++ get_exercises.rb
@@ -8,5 +8,13 @@ reshuege = Reshuege.new('https://ege.sdamgia.ru/test?theme=168')
 exercises = reshuege.get
 
 exercises.each do |exercise|
+  exercise = exercise.split(' ').map { |w| w.gsub(/[\u0080-\u00ff]/, '') }.join(' ')
+  # p exercise
   Exercise.find_or_create_by(content: exercise)
+  # p test.encode("UTF-8", "Windows-1252")
+  # test = test.unpack("U*").map{|c|c.chr rescue '_' }.join
+  # test = test.unpack("U*")
+  # p test[3].encoding
+  # p test.each_char.select{|c| c.bytes.count < 4 }.join('')
+  # exit
 end
--- settings/config.rb
+++ settings/config.rb
@@ -4,11 +4,23 @@ require 'active_record'
 
 require "./lib/db/exercise"
 
+require 'active_record/connection_adapters/abstract_mysql_adapter'
+
+module ActiveRecord
+  module ConnectionAdapters
+    class AbstractMysqlAdapter
+      NATIVE_DATABASE_TYPES[:string] = { :name => "varchar", :limit => 191 }
+    end
+  end
+end
+
 ActiveRecord::Base.establish_connection(
         :adapter => "mysql2",
         :host => "localhost",
         :username => "root",
         :password => "180489",
         :database => "read_exercise",
-        :encoding => "utf8",
+        :encoding => "utf8mb4",
         socket: '/opt/lampp/var/mysql/mysql.sock')
+
+# ActiveRecord::Base.connection.execute("ALTER TABLE read_exercise MODIFY content TEXT CHARACTER SET utf8mb4 COLLATE utf8mb4_bin;")
